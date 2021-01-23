f230bc9ea0bc49674d39923efcd404923919e9cf
İsmail Gümüşmadenoğlu <igmo@outlook.com.tr>
2021-01-23 15:38:34 +0300

    Add project files.

---
+++ MyDictonaryHomework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary=new MyDictionary<int, string>();
+            myDictionary.Add(1,"Emine");
+            myDictionary.Add(2,"Birce");
+            myDictionary.Add(3,"İsmail");
+        }
+    }
+}
